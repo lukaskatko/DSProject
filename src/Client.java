12223f61e032d1439bd84c3622046ba7347da1cf
alitzerman <66385358+alitzerman@users.noreply.github.com>
2021-08-14 15:08:05 -0400

    Add files via upload

--- src/Client.java
+++ src/Client.java
@@ -16,11 +16,17 @@ public class Client {
 		System.out.println(menu.menu());
 		
 		//CONNECTION TO SERVERS TO BE IMPLEMENTED
+		
+		
 		Scanner sc = new Scanner(System.in);
         String input = "";
-        while (!input.equals("end")) {
+        while (!input.toLowerCase().equals("exit")) {
         	System.out.print("Enter command: ");
         	input = sc.nextLine();
+        	if (input.equals("exit")) {
+        		System.out.println("Goodbye");
+        		break;
+        	}
         	Parser x = new Parser();
         	int command =  x.Parse(input);
         	if (command == 0 ) {
@@ -28,16 +34,91 @@ public class Client {
         	}
         	command = Integer.parseInt(input);
         	if (command == 1) {
-        		//CREATE ACCOUNT
+        		input = "";
+        		System.out.print("To create your account, we will need a first and last name");
+        		System.out.print("Please enter First Name:");
+        		String first = sc.nextLine();
+        		System.out.print("Please enter Last Name:");
+        		String last = sc.nextLine();
+        		
+        		//PUT FIRST and LAST name into DB // GET BACK ACCOUNT NUMBER
+        		
         	}
         	if (command == 2) {
-        		//GET BALANCE
+        		input = "";
+        		System.out.print("To get your balance, we will need your First name, Last name, and Account number");
+        		System.out.print("Please enter First Name:");
+        		String first = sc.nextLine();
+        		System.out.print("Please enter Last Name:");
+        		String last = sc.nextLine();
+        		System.out.print("Please enter Account Number:");
+        		String acctNum = sc.nextLine();
+        		
+        		//GET BALANCE FROM HERE
         	}
         	if (command == 3) {
-        		//WITHDRAW FUNDS
+        		input = "";
+        		System.out.println("To withdraw we will need your First name, Last name, and Account number");
+        		System.out.println("Please enter First Name:");
+        		String first = sc.nextLine();
+        		System.out.println("Please enter Last Name:");
+        		String last = sc.nextLine();
+        		System.out.println("Please enter Account Number:");
+        		String acctNum = sc.nextLine();
+        		
+        		//LOGIC CHECK HERE TO SEE IF FIRST/LAST/ACCTNUM MATCH A ROW IN DB ///
+        		
+        		System.out.println("Please enter Amount to withdraw:");
+        		double withdrawAmount = 0;
+        		while (withdrawAmount == 0) {
+        			acctNum = sc.nextLine();
+        			try {
+        				withdrawAmount = Double.parseDouble(acctNum);
+        				if (withdrawAmount <= 0) {
+        					System.out.println("Amount must be positive");
+        					withdrawAmount = 0;
+        					System.out.println("Please enter amount to withdraw");
+        				}
+        			}catch (Exception e) {
+        				System.out.println("Please enter a valid $$ amount in $$.$$ format.");
+        			}
+        		}
+        		
+        		//////////WITHDRAW LOGIC HERE ///////
+        		
+        		
+        
         	}
         	if (command == 4) {
-        		//DEPOSIT FUNDS
+        		input = "";
+        		System.out.println("To Deposit we will need your First name, Last name, and Account number");
+        		System.out.println("Please enter First Name:");
+        		String first = sc.nextLine();
+        		System.out.println("Please enter Last Name:");
+        		String last = sc.nextLine();
+        		System.out.println("Please enter Account Number:");
+        		String acctNum = sc.nextLine();
+        		
+        		//LOGIC CHECK HERE TO SEE IF FIRST/LAST/ACCTNUM MATCH A ROW IN DB ///
+        		
+        		System.out.println("Please enter Amount to deposit:");
+        		double depositAmount = 0;
+        		while (depositAmount == 0) {
+        			acctNum = sc.nextLine();
+        			try {
+        				depositAmount = Double.parseDouble(acctNum);
+        				if (depositAmount <= 0) {
+        					System.out.println("Amount must be positive");
+        					depositAmount = 0;
+        					System.out.println("Please enter amount to deposit:");
+        				}	
+        			}catch (Exception e) {
+        				System.out.println("Please enter a valid $$ amount in $$.$$ format.");
+        			}
+        		}
+        		
+        		//////////DEPOSIT LOGIC HERE ///////
+
         	}
         	if (command == 5) {
         		System.out.println(menu.menu());
