28ff6ff39b84720f254d3eaef12c4972c40cd61d
alitzerman <66385358+alitzerman@users.noreply.github.com>
2021-08-15 11:36:23 -0400

    Add files via upload

--- src/AccountDao.java
+++ src/AccountDao.java
@@ -7,17 +7,19 @@ import java.rmi.RemoteException;
  * user.
  *
  */
-public interface AccountDao extends Remote{
-	
+public interface AccountDao extends Remote {
+
 	long createUser(String firstName, String lastName, String userName) throws RemoteException;
-	
-	boolean deposit(long accountNumber, String userName, double amount) throws RemoteException ;
-	boolean depositFor2PC(long accountNumber, String userName, double amount) throws RemoteException ;
+
+	boolean deposit(long accountNumber, String userName, double amount) throws RemoteException;
+
+	boolean depositFor2PC(long accountNumber, String userName, double amount) throws RemoteException;
 
 	double withDraw(long accountNumber, String userName, double amount) throws RemoteException;
+
 	double withDrawFor2PC(long accountNumber, String userName, double amount) throws RemoteException;
 
 	Account getBalance(long accountNumber, String userName) throws RemoteException;
 
-	Integer requestToPublishAtServer();
+	Integer requestToPublishAtServer() throws RemoteException;
 }
